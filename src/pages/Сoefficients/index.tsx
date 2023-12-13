bf8427d0c849ee73c7e2536a03670c1b54589b02
AlickSadler <alicksadler@gmail.com>
2023-12-13 13:54:35 +0200

    'push'

--- src/pages/Сoefficients/index.tsx
+++ src/pages/Сoefficients/index.tsx
@@ -6,16 +6,24 @@ import styles from './Coefficients.module.scss';
 
 const Coefficients = () => {
 	const [coefficients, setCoefficients] = useState<string[]>([]);
+	const [isReload, setIsReload] = useState<boolean>(false);
+
+	useEffect(() => {
+		window.addEventListener('click', () => {
+			setIsReload(!isReload);
+		});
+	}, []);
 	useEffect(() => {
 		fetch('https://lucky-cromium-cfltw.run-eu-central1.goorm.site/coefficients')
 			.then(res => res.json())
 			.then((data: string[]) => {
 				setCoefficients(data);
 			});
-	}, []);
+	}, [isReload]);
 
 	return (
 		<div className={styles.coefficients}>
+			<div className={styles.title}>История раундов</div>
 			<div className={styles.coefficientsWrapper}>
 				{!coefficients.length && 'Загрузка...'}
 				{coefficients.map(coefficient => {
